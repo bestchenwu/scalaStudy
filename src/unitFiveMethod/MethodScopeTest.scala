b0f0b029060db4d04026ccaa400493bd5a3fc92a
bestchenwu <bestchenwu@163.com>
2018-10-05 18:05:54 +0800

    增加第三章控制结构和第五章类方法的学习

---
+++ src/unitFiveMethod/MethodScopeTest.scala
@@ -0,0 +1,10 @@
+package unitFiveMethod
+
+object MethodScopeTest {
+    
+   def main(stringArray:Array[String]):Unit={
+       var methodScope = new MethodScope;
+       //仅当前类作用
+       //methodScope.privateMethod;
+   }
+}
---
+++ src/unitThreeString/WhileListDemo.scala
@@ -0,0 +1,27 @@
+package unitThreeString
+
+import scala.annotation.tailrec
+
+object WhileListDemo {
+    
+    /**
+     * 自定义循环控制机构
+     */
+    @tailrec
+    def whileList(testCondition: =>Boolean)(codeBlock: =>Unit){
+        if(testCondition){
+          codeBlock;
+          whileList(testCondition)(codeBlock)
+        }
+    }
+    
+    def main(args: Array[String]): Unit = {
+      var x = 1; 
+      var sum = 0;
+      whileList(x<5){
+          sum+=x;          
+          x +=1;
+      }
+      println("sum="+sum);
+    }
+}
