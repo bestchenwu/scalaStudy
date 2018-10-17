dca8f7f1f5b60f1630710dd35d384344ce0fe8e4
chenwu <chenwu@meitunmama.com>
2018-10-17 20:30:21 +0800

    提交集合运算

--- src/unitTen/UnionTest.scala
+++ src/unitTen/UnionTest.scala
@@ -2,6 +2,12 @@ package src.unitTen
 
 object UnionTest extends App{
 
-  val a = Array(1,2)
-  //a++=Seq(3,4)
+//  var a = Array(1,2)
+//  //a ++= Seq(3,4)
+//   var b = a++Seq(3,4)
+//  b.foreach(println)
+    var a = Set(1,2,3)
+  var b = Set(1,2,3,4)
+   var c = b -- a
+  println(c)
 }
