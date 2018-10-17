f4e16daaa2f6bc4eba5eb7d0b52f4e9f87e54eea
bestchenwu <bestchenwu@163.com>
2018-10-17 23:24:32 +0800

    增加注释

--- src/unitTen/UnionTest.scala
+++ src/unitTen/UnionTest.scala
@@ -1,13 +1,14 @@
 package src.unitTen
 
 object UnionTest extends App{
-
+    //++运算符可以用在序列或者链表里
 //  var a = Array(1,2)
 //  //a ++= Seq(3,4)
 //   var b = a++Seq(3,4)
 //  b.foreach(println)
-    var a = Set(1,2,3)
+  var a = Set(1,2,3)
   var b = Set(1,2,3,4)
+   //--运算符是由Subtractable特质提供的，目前实现该特质的是Set、 map、Buffer
    var c = b -- a
-  println(c)
+    println(c)
 }
